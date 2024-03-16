801b724253e281cd6be4791cb0a7f2c4a03d0193
ZENNI <134192225+zenitsu14052005@users.noreply.github.com>
2024-03-17 01:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ const client = new Discord.Client({
 });
 
 client.on('ready', async () => {
-  const rpc = new Discord.RichPresence()
+  const rpc = new Discord.RichPresence({
+    startTimestamp: Date.now()
+  })
     .setApplicationId('1110523342479425636')
     .setType('STREAMING')
     .setURL(`https://www.youtube.com/watch?v=S2TaAcwC_zI`)
@@ -44,10 +46,11 @@ client.on('ready', async () => {
       current: 4,
       id: Discord.getUUID(),
     })
-    .setAssetsLargeImage(`mp:attachments/1178850195233063003/1217581881470750851/2ecb86d208eff8ec407dac3c09256c7b.gif?ex=66048c7e&is=65f2177e&hm=0e586cb343ecd43b4b5c9967de5b7c343741781c404aacdd326d01de80caa792&`)
+    .setAssetsLargeImage(`mp:attachments/1158679802056810506/1218443036154138695/f9149157eb6b90cc828ae1e183c82c05.gif?ex=6607ae81&is=65f53981&hm=8e59959d3ab3c0409d8b4e983556eb476044c0ee21a814db0ac4b5358ca9036f&`)
     .setAssetsLargeText(`crazy`)
     .addButton(`BRUTAL BEERUS`, `https://discord.gg/C6TWAAWAU9`)
-    .addButton(`ZENITSU</3`, `https://discord.gg/zenni-realm-873124938435944488`)
+    .addButton(`ZENITSU</3`, `https://discord.gg/CBgvQtYhVf`);
+
   client.user.setActivity(rpc.toJSON());
   console.log(`${client.user.tag} is ready!`);
 });
